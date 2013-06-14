4668ade5ac134f00059d62f8ef7c67be19627d67
Will Trimble <trimble@anl.gov>
2013-06-14 11:22:01 -0500

    tweaking test parameters

--- tests/invoke-tests.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-~/EHFI/plot_pco_with_stats_all.3-4-13.pl -f o-0.0001 -g o.groups -m euclidean -t dataset_rand -z qiime_pipe -q qiime_table -o test_a   # -cleanup
-~/EHFI/plot_pco_with_stats_all.3-4-13.pl -f o-0.0001 -g o.groups -m euclidean -t rowwise_rand -z qiime_pipe -q qiime_table -o test_b   # -cleanup
-
-
