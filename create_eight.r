c32b5eee203dc2e0517620b3c12f59fbf9b9f975
Kevin Keegan <kevin.p.keegan@gmail.com>
2013-08-09 14:53:56 -0500

    edit create_eight.r

--- create_eight.r
+++ create_eight.r
@@ -237,7 +237,7 @@ create_eight <- function(
     
     # import data
     print("     importing data ...")
-    raw_counts.matrix <<- import_data(counts_file) # (output_3)
+    raw_counts.matrix <- import_data(counts_file) # (output_3)
     my_id.matrix <- import_data(percent_file)
     print("     importing data DONE")
 
@@ -248,7 +248,7 @@ create_eight <- function(
     print("     checking agreement of counts and percentid files DONE")
     
     # raw counts with singletons removed (output_1)
-    raw_counts.singletons_rm.matrix <<- remove_singletons(raw_counts.matrix, abundance_limit = 1, tag="make_out_1")
+    raw_counts.singletons_rm.matrix <- remove_singletons(raw_counts.matrix, abundance_limit = 1, tag="make_out_1")
     #rownames(raw_counts.singletons_rm.matrix) <- rownames(raw_counts.matrix)
     #colnames(raw_counts.singletons_rm.matrix) <- colnames(raw_counts.matrix)
 
@@ -256,7 +256,7 @@ create_eight <- function(
     normed_counts.singletons_rm.matrix <- norm_center_scale(raw_counts.singletons_rm.matrix, tag="make_out_2")
 
     # normalized raw counts with singletons included (output_4)
-    normed_counts.matrix <- norm_center_scale(raw_counts.matrix, tag="make_out_4")
+    normed_counts.matrix <<- norm_center_scale(raw_counts.matrix, tag="make_out_4")
 
     # counts that are the same or greater than percent_screen (filtered to remove rows that sum to 0) # make output_7
     raw_counts.pass_screen.matrix <- matrix(0,dim(raw_counts.matrix)[1],dim(raw_counts.matrix)[2]) 
