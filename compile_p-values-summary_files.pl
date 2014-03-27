3885049c0bcde2d0045151f1999cf08ebb34a324
Kevin Keegan <kevin.p.keegan@gmail.com>
2014-03-27 12:03:32 -0500

    use compile function to organize outputs a little beeter

--- compile_p-values-summary_files.pl
+++ compile_p-values-summary_files.pl
@@ -75,7 +75,8 @@ foreach my $file (@file_list){ # process each file
   my $scaled_dists_out = $file;
   my $dist_ps_out = $file;
   my $num_perms_out = $file;
-  
+ 
+  print STDOUT "\n"."MADE IT HERE (1)"."\n";
   open(FILE, "<", $target_dir.$file) or die "can't open FILE $target_dir$file"; 
   while (my $line = <FILE>){
     
@@ -167,7 +168,7 @@ foreach my $file (@file_list){ # process each file
   my $move_p_summaries_string = "mv *.P_VALUE_SUMMARY $pcoa_p_summary_dir";
   system($move_p_summaries_string)==0 or die "died running"."\n".$move_p_summaries_string."\n";
 
-  print STDOUT "\n"."MADE IT HERE"."\n";
+  print STDOUT "\n"."MADE IT HERE (2)"."\n";
 
 }
     
