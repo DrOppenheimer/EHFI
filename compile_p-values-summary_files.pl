f6084b3403bee21fc96c1efd6e700d40665e9afc
Kevin Keegan <kevin.p.keegan@gmail.com>
2014-04-02 17:07:26 -0500

    debug compile function

--- compile_p-values-summary_files.pl
+++ compile_p-values-summary_files.pl
@@ -183,7 +183,7 @@ if ( $sort_output ){
   }
 
   # this one's a little more complicate - copies the individual result folders to the summary dir
-  my $list_individual_results_string = "ls -d *.RESULTS> individual_results_list"
+  my $list_individual_results_string = "ls -d *.RESULTS> individual_results_list";
   system($list_individual_results_string)==0 or die "died running"."\n".$list_individual_results_string."\n";
   my $copy_individual_results_string = "for i in `cat individual_results_list`; do mv $i $individual_results_dir; done";
   system($copy_individual_results_string)==0 or die "died running"."\n".$copy_individual_results_string."\n";
