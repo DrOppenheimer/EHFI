1bf81f9946babc42e65e4a2e9aeaf2fea3ecd3cd
Kevin Keegan <kevin.p.keegan@gmail.com>
2014-04-25 16:29:26 -0500

    added revised pcoa render script; dev.v3

--- combine_summary_stats.pl
+++ combine_summary_stats.pl
@@ -285,7 +285,7 @@ if( $groups_list ){
   #      legend_width_scale(0.2) pcoa_width_scale(0.8) legend_cex(0.5) figure_cex(0.7)
   system($render_pcoa_string)==0 or die "died running"."\n".$render_pcoa_string."\n";
   # now copy image back to results before zipping
-  my $copy_rendered_pcoa_string = "cp $job_name.PCoA.pcoa.png $results_dir/$job_name.PCoA.pcoa.png"; 
+  my $copy_rendered_pcoa_string = "cp $job_name.$groups_list.png $results_dir/$job_name.$groups_list.png"; 
   print LOG "copy PCoA image back to results:"."\n".$copy_rendered_pcoa_string."\n";
   system($copy_rendered_pcoa_string)==0 or die "died running"."\n".$copy_rendered_pcoa_string."\n";
 
