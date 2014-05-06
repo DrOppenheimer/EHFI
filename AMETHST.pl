c5d0d4db094d001cdc8d81f5927e35847b41b522
Kevin Keegan <kevin.p.keegan@gmail.com>
2014-05-05 19:10:09 -0500

    Bin to RealBin; AMETHST.pl

--- AMETHST.pl
+++ AMETHST.pl
@@ -91,19 +91,7 @@ while (my $line = <QIIME_ACTIVATION>){
 # Add the R and AMETHST path information to beginning of path
 $ENV{PATH} = "$r_path:$amethst_path:$ENV{PATH}";
 
-# define some variables from the commands file
-$command_file = basename($command_file);
-my $current_dir = getcwd()."/";
-my $script_dir = "$FindBin::RealBin/";
-my $path_file = $current_dir.$command_file;
-#my $log_file = $current_dir.$command_file.".MASTER.log";
-my $log_prefix = "my_log";  
-my $job_counter = 1;
-my $log_file = $current_dir.$command_file.".".$start_time_stamp.".log";
 
-# Open the log for printing
-open(LOG, ">", $log_file) or die "cant open LOG $log_file"."\n";
-print LOG "Start: ".$start_time_stamp."\n";
 
 ########### Run if -c option (AWE summary) is invoked ###########
 if ($awe_compile_summary){
@@ -120,12 +108,26 @@ if ($awe_compile_summary){
 
   # compile the data
   my $compile_summary_string = "compile_p-values-summary_files.pl --sort_output --output_zip=$summary_name";
-  print LOG "\n\n"."Running compile_p-values-summary_files.pl:"."\n".$compile_summary_string."\n"; 
+  #print LOG "\n\n"."Running compile_p-values-summary_files.pl:"."\n".$compile_summary_string."\n"; 
   system($compile_summary_string)==0 or die "dies running:"."\n".$compile_summary_string."\n" ;
 #################################################################  
 }else{
 
   ######### PERFORM AMETHST ANALYSIS ###########
+
+  # define some variables from the commands file
+  $command_file = basename($command_file);
+  my $current_dir = getcwd()."/";
+  my $script_dir = "$FindBin::RealBin/";
+  my $path_file = $current_dir.$command_file;
+  #my $log_file = $current_dir.$command_file.".MASTER.log";
+  my $log_prefix = "my_log";  
+  my $job_counter = 1;
+  my $log_file = $current_dir.$command_file.".".$start_time_stamp.".log";
+  
+  # Open the log for printing
+  open(LOG, ">", $log_file) or die "cant open LOG $log_file"."\n";
+  print LOG "Start: ".$start_time_stamp."\n";
 
   # try to detect the number of CPUS
   my $num_cpus=`nproc`;
