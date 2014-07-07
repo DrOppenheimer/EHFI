94f434d64c525be9103c2962bedb723e3bda61f9
Kevin Keegan <kevin.p.keegan@gmail.com>
2014-07-07 17:52:56 -0500

    added simple test script test_amethst.sh

--- test_amethst.sh
+++ test_amethst.sh
@@ -1,16 +1,32 @@
 #!/bin/bash                                                                                                                                              
 # Simple script to test AMETHST, if this test complete's without errors, AMETHST is working                                                              
 # move to the directory with AMEHST test data
-cd ./datasets/test_analysis_data
 
+# get path for AMETHST
+START_DIR=`pwd`
+WHICH_AMETHST=`which amethst.pl`
+AMETHST_PATH=$(dirname ${WHICH_AMETHST})
+TEST_DATA_PATH=$AMETHST_PATH"/datasets/test_analysis_data/"
+
+echo "This is a simple test for AMETHST functionaility - if it complete's without errors, AMETHST is properly installed"
+
+echo "Moving to: "$TEST_DATA_PATH
+
+cd $TEST_DATA_PATH
+
+echo "running AMETHST test"
 # run the test
 AMETHST.pl -f test_analysis_commands -k -z
 
+echo "running post test cleanup"
 # perform post test cleanup
 rm *log
 rm -R AMETHST.Summar*
 rm AMETHST.All_data.tar.gz
 rm *list*
+
+echo "moving back to: $START_DIR"
+cd $START_DIR
 
 # print simple message
 echo "If no errors were displayed, AMETHST is properly installed"
